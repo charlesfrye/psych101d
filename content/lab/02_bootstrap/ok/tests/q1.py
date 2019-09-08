a698a1087e8ddef7bc6b358ccc28cf80ddb5d7e1
Charles Frye <charlesfrye@berkeley.edu>
2019-09-08 10:52:52 -0700

    adds autograder to lab02

---
+++ content/lab/02_bootstrap/ok/tests/q1.py
@@ -0,0 +1,24 @@
+test = {
+    "name": "Computing the prevalence of a preference",
+    "points": 1,
+    "suites": [
+        {
+            "cases": [
+                {
+                    "code": r"""
+                    >>> ## did you define the right variable?
+                    >>> "observed_E_prevalence" in globals().keys()
+                    True
+                    >>> ## does it have the right value?
+                    >>> observed_E_prevalence
+                    0.208
+                    """,
+                    "hidden": False,
+                    "locked": False
+                }
+            ],
+            "setup": r"""""",
+            "teardown": r"""
+            """,
+            "type": "doctest"}]
+        }
---
+++ content/lab/02_bootstrap/ok/tests/q2.py
@@ -0,0 +1,24 @@
+test = {
+    "name": "Computing the prevalence of a type",
+    "points": 1,
+    "suites": [
+        {
+            "cases": [
+                {
+                    "code": r"""
+                    >>> ## did you define the right variable?
+                    >>> "observed_INTJ_prevalence" in globals().keys()
+                    True
+                    >>> ## does it have the right value?
+                    >>> observed_INTJ_prevalence
+                    0.12
+                    """,
+                    "hidden": False,
+                    "locked": False
+                }
+            ],
+            "setup": r"""""",
+            "teardown": r"""
+            """,
+            "type": "doctest"}]
+        }
