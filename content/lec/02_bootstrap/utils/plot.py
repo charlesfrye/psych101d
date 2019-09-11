5bffdd99e4cdc8424059f7f1010db336eaa09b6f
Charles Frye <charlesfrye@berkeley.edu>
2019-09-11 15:51:21 -0700

    adds material for lecture02b, on confidence intervals and bootstrap

--- content/lec/02_bootstrap/utils/plot.py
+++ content/lec/02_bootstrap/utils/plot.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import seaborn as sns
 
@@ -34,6 +35,47 @@ def plot_score_comparison(data, guesses, first_game_scores, second_game_scores, 
     axs[1].set_ylim(*first_game_ylims); axs[2].set_ylim(*second_game_ylims);
     axs[2].set_xlabel("Guess");
     axs[1].set_ylabel("Score"); axs[2].set_ylabel("Score");
+    
+    
+def plot_spread_prediction(center, sample_sizes, scaling=2, label=None, ax=None):
+    """The Law of Large Numbers predicts that the 
+    """
+    if ax is None:
+        f, ax = plt.subplots(figsize=(10, 4))
+        
+    spread_scales = scaling * 1 / np.sqrt(sorted(sample_sizes))
+    ax.plot(sorted(sample_sizes), center + spread_scales, color="C1", label=label);
+    ax.plot(sorted(sample_sizes), center - spread_scales, color="C1", label=label);
+    return ax
+
+
+def plot_samples(sample_sizes, observed_values, alpha=0.1, label=None, ylabel="", ax=None):
+    if label is None:
+        label = "Observed Values"
+    if ax is None:
+        f, ax = plt.subplots(figsize=(10, 4))
+    
+    ax.scatter(sample_sizes, observed_values, alpha=alpha, label=label);
+    ax.set_xlabel("Sample Size"), ax.set_ylabel(ylabel)
+    return ax
+
+
+def make_LLN_plot(sample_sizes, observed_stat_values, true_stat_value, ylabel="",
+                  spread_center=None, spread_scaling=2, spread_label=None, alpha=0.1):
+    """Creates a plot demonstrating the Law of Large Numbers in action.
+    
+    1. Plots the observed statistic values against sample sizes with plot_samples
+    2. Plots the predictions for the spread of observed values with plot_spread_prediction
+    """
+    if spread_center is None:
+        spread_center = true_stat_value
+    
+    f, ax = plt.subplots(figsize=(10, 4))
+    plot_samples(sample_sizes, observed_stat_values, alpha, ylabel=ylabel, ax=ax)
+    plot_spread_prediction(spread_center, sample_sizes, spread_scaling, label=spread_label, ax=ax)
+    ax.hlines(true_stat_value, 0, 0.25 * max(sample_sizes), lw=2, color="C3", label="Value on Population");
+    ax.legend(loc="upper right");
+    return ax
     
     
 def add_mean_median(mean, median, lims, ax, mean_color="C1", median_color="C0"):
